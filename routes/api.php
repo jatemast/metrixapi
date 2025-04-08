5cc2513f6997501c7f78757e8096d0ca57431d6f
javier teheran magallanez <141957021+jatemast@users.noreply.github.com>
2025-04-08 16:44:01 -0500

    reparacion de ruta

--- routes/api.php
+++ routes/api.php
@@ -10,11 +10,9 @@ use App\Http\Controllers\AuthController;
 
 Route::post('/registro', [AuthController::class, 'register']);
 Route::post('/inicio-sesion', [AuthController::class, 'login']);
-Route::middleware(['auth:sanctum', 'role:admin'])->group(function () {
-    Route::get('/admin/dashboard', function () {
-        return response()->json(['message' => 'Bienvenido administrador']);
-    });
-});
+ 
+ 
+
 
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
